6b3c4deb0e08bc4a1dad872b00efa96069ac3e9a
yanghan-a <12112522@mail.sustech.edu.cn>
2025-12-28 11:20:52 +0800

    添加楼梯地形的gym入口

--- exts/bipedal_locomotion/bipedal_locomotion/tasks/locomotion/robots/__init__.py
+++ exts/bipedal_locomotion/bipedal_locomotion/tasks/locomotion/robots/__init__.py
@@ -1,6 +1,6 @@
 import gymnasium as gym
 
-from bipedal_locomotion.tasks.locomotion.agents.limx_rsl_rl_ppo_cfg import PF_TRON1AFlatPPORunnerCfg, WF_TRON1AFlatPPORunnerCfg, SF_TRON1AFlatPPORunnerCfg, PF_TRON1ARoughPPORunnerCfg
+from bipedal_locomotion.tasks.locomotion.agents.limx_rsl_rl_ppo_cfg import PF_TRON1AFlatPPORunnerCfg, WF_TRON1AFlatPPORunnerCfg, SF_TRON1AFlatPPORunnerCfg, PF_TRON1ARoughPPORunnerCfg, PF_TRON1AStairPPORunnerCfg
 
 from . import limx_pointfoot_env_cfg, limx_wheelfoot_env_cfg, limx_solefoot_env_cfg
 
@@ -11,6 +11,8 @@ from . import limx_pointfoot_env_cfg, limx_wheelfoot_env_cfg, limx_solefoot_env_
 limx_pf_blind_flat_runner_cfg = PF_TRON1AFlatPPORunnerCfg()
 
 limx_pf_blind_rough_runner_cfg = PF_TRON1ARoughPPORunnerCfg()
+
+limx_pf_blind_stair_runner_cfg = PF_TRON1AStairPPORunnerCfg()
 
 limx_wf_blind_flat_runner_cfg = WF_TRON1AFlatPPORunnerCfg()
 
@@ -63,6 +65,27 @@ gym.register(
     kwargs={
         "env_cfg_entry_point": limx_pointfoot_env_cfg.PFBlindRoughEnvCfg_PLAY,
         "rsl_rl_cfg_entry_point": limx_pf_blind_rough_runner_cfg,
+    },
+)
+
+#点足机器人楼梯训练接口
+gym.register(
+    id="Isaac-Limx-PF-Blind-Stairs-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": limx_pointfoot_env_cfg.PFBlindStairEnvCfg,
+        "rsl_rl_cfg_entry_point": limx_pf_blind_stair_runner_cfg,
+    },
+)
+
+gym.register(
+    id="Isaac-Limx-PF-Blind-Stairs-Play-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": limx_pointfoot_env_cfg.PFBlindStairEnvCfg_PLAY,
+        "rsl_rl_cfg_entry_point": limx_pf_blind_stair_runner_cfg,
     },
 )
 
